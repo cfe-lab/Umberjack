80252cbded48ff3167e19bff77bf15ce5745567b
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2014-04-22 19:16:16 -0700

    changed variable names to make codons vs bases more clear.  Added site column in sample_genome.rates

--- test/simulations/sample_genomes.py
+++ test/simulations/sample_genomes.py
@@ -4,8 +4,13 @@ Create genome alignment by random sampling columns from INDELible simulation out
 import sys
 from seqUtils import *
 import random
+import collections
 
-root = '/Users/apoon/wip/sliders/simulation/'
+
+BASES_PER_CODON = 3
+INDELIBLE_SITE_INFO_START = 11  # The line that the site information actually starts on.  Sites are 1-based
+
+root = './data/'
 
 global_scaling_factors = [10.0, 100.0]
 
@@ -26,43 +31,60 @@ n_sites = 3000 # target size of genome in codons
 # make up a genome with five intervals 
 # mutation rate factors 1x, 10x, 100x, 10x, 1x 
 # 500, 500, 1000, 500, 500 codons
-scaling_factors = [1.0, 2.0, 5.0, 10.0, 100.0, 10.0, 5.0, 2.0, 1.0]
-lengths = [100, 200, 200, 500, 1000, 500, 200, 200, 100]  # codons
 
-left = 0  # gets updated with each interval
+Interval = collections.namedtuple(typename='Interval', field_names=['scaling_factor', 'num_codons'], verbose=True)
+intervals = [Interval(scaling_factor=1.0, num_codons=100),
+             Interval(scaling_factor=2.0, num_codons=200),
+             Interval(scaling_factor=5.0, num_codons=200),
+             Interval(scaling_factor=10.0, num_codons=500),
+             Interval(scaling_factor=100.0, num_codons=1000),
+             Interval(scaling_factor=10.0, num_codons=500),
+             Interval(scaling_factor=5.0, num_codons=200),
+             Interval(scaling_factor=2.0, num_codons=200),
+             Interval(scaling_factor=1.0, num_codons=100)]
+#scaling_factors = [1.0, 2.0, 5.0, 10.0, 100.0, 10.0, 5.0, 2.0, 1.0]
+#codon_interval_lengths = [100, 200, 200, 500, 1000, 500, 200, 200, 100]  # codons
 
+start_codon_0based = 0  # gets updated with each interval
+new_codon_site_0based = 0
 new_fasta = {}
 
-ratefile = open('sample_genomes.rates', 'w')  # keep track of each codon site omega
+ratefile = open(root + 'sample_genomes.rates', 'w')  # keep track of each codon site omega
+ratefile.write('Site,Interval,Scaling_factor,Rate_class,Omega\n')
 
-for i, scaling_factor in enumerate(scaling_factors):
-    # read FASTA
-    infile = open(root+'version1/scaling_'+str(scaling_factor)+'.fas', 'rU')
-    fasta = convert_fasta(infile.readlines())  # returns a List of header, sequence lists
+for interval_idx, interval in enumerate(intervals):
+    scaling_factor = interval.scaling_factor
+    num_codons = interval.num_codons
+    # read INDELible tree sequence FASTA
+    # scaling_<scaling_factor>_TRUE.fas are fastas containing the INDELible node sequences of a phylogenetic tree
+    # The tree mutation rate is scaled by <scaling_factor>.
+    infile = open(root+'scaling_'+str(scaling_factor)+'_TRUE.fas', 'rU')
+    fasta = convert_fasta(infile.readlines())  # returns a List of (header, sequence) tuples
     infile.close()
     
-    # if this is first time, transfer headers
+    # if this is first time, transfer header
     if len(new_fasta) == 0:
         new_fasta = dict([(h, '') for h, s in fasta])
     
     # transfer intervals
     for h, s in fasta:
-        new_fasta[h] += s[ (3*left) : (3*(left+lengths[i])) ]  # in bps
+        new_fasta[h] += s[ (BASES_PER_CODON*start_codon_0based) : (BASES_PER_CODON*(start_codon_0based+num_codons)) ]  # in bps
     
     # read rates, starts on line 11 (10 in zero-index)
-    infile = open(root+'version1/scaling_'+str(scaling_factor)+'_RATES.txt', 'rU')
+    infile = open(root+'scaling_'+str(scaling_factor)+'_RATES.txt', 'rU')
     lines = infile.readlines()
     infile.close()
-    for line in lines[(10+left):(10+left+lengths[i])]:
-        site, rate_class, partition = line.strip('\n').split('\t')
+    for line in lines[(INDELIBLE_SITE_INFO_START+start_codon_0based):(INDELIBLE_SITE_INFO_START+start_codon_0based+ num_codons)]:
+        codon_site, rate_class, partition = line.strip('\n').split('\t')
         rate_class = int(rate_class)
-        ratefile.write('%d,%1.1f,%d,%f\n' % (i, scaling_factor, rate_class, omegas[rate_class]))
+        new_codon_site_0based += 1
+        ratefile.write('%d,%d,%1.1f,%d,%f\n' % (new_codon_site_0based, interval_idx, scaling_factor, rate_class, omegas[rate_class]))
     
-    left += lengths[i]  # update left bound
+    start_codon_0based += num_codons  # update left bound
     
 
 # output            
-outfile = open('sample_genomes.fas', 'w')
+outfile = open(root+'sample_genomes.fas', 'w')
 
 for h in new_fasta.iterkeys():
     outfile.write('>%s\n%s\n' % (h, new_fasta[h]))
