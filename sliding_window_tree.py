238565400fa1a2df49a684649742c99041c4d3bb
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2014-04-23 19:22:51 -0700

    Add logging for timing

--- sliding_window_tree.py
+++ sliding_window_tree.py
@@ -17,7 +17,7 @@ FASTTREE_EXE = "/home/thuy/programs/fasttree/FastTree"
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter('%(asctime)s - [%(levelname)s] [%(name)] %(message)s', datefmt="%Y-%m-%d %H:%M:%S.%f")
+formatter = logging.Formatter('%(asctime)s - [%(levelname)s] [%(name)s] %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
 console_handler.setFormatter(formatter)
 LOGGER.addHandler(console_handler)
         
@@ -44,9 +44,11 @@ def process_windows(ref_fasta_filename, sam_filename, out_dir, mapping_cutoff, r
     msa_fasta_filename = out_dir + os.sep + sam_filename_prefix + ".msa.fasta"
 
     # TODO:  need to create separate msa fasta file for each reference contig/chromosome
+    LOGGER.debug("Start Full MSA-Fasta from SAM")
     sam_handler.create_msa_fasta_from_sam(sam_filename=sam_filename, ref_fasta_filename=ref_fasta_filename,
                                        mapping_cutoff=mapping_cutoff, read_qual_cutoff=read_qual_cutoff, max_prop_N=max_prop_N,
                                        out_fasta_filename=msa_fasta_filename)
+    LOGGER.debug("Done Full MSA-Fasta from SAM")
 
     #  Get the best window size
     # TODO:  handle multiple contig/chromosome in reference fasta.  Right now only handles one contig/chromosome.
@@ -63,16 +65,21 @@ def process_windows(ref_fasta_filename, sam_filename, out_dir, mapping_cutoff, r
             end_nucpos = start_nucpos + windowsize                # end_nucpos is 1-based nucleotide position
 
             # Slice the multiple sequence aligned fasta file into a window fasta
+            LOGGER.debug("Start Create Sliced MSA-Fasta " + str(start_nucpos) + "-" + str(end_nucpos))
             msa_window_fasta_filename = out_dir + os.sep + sam_filename_prefix + ".msa." + str(start_nucpos) + "_" + str(end_nucpos) + ".fasta"
             total_slice_seq = slice_miseq.create_slice_msa_fasta(fasta_filename=msa_fasta_filename,
                                                                  out_fasta_filename=msa_window_fasta_filename,
                                                                  start_pos=start_nucpos, end_pos=end_nucpos,
                                                                  breadth_thresh=window_breadth_thresh)
+            LOGGER.debug("Done Create Sliced MSA-Fasta " + str(start_nucpos) + "-" + str(end_nucpos) +
+                         ".  Wrote " + str(total_slice_seq) + " to file")
 
             if total_slice_seq < window_depth_thresh:
-                LOGGER.warn("Window " + start_nucpos + "-" + end_nucpos + " does satisfy window depth constraints")
+                LOGGER.warn("Window " + str(start_nucpos) + "-" + str(end_nucpos) + " does not satisfy window depth constraints")
             else:
                 # Feed window fasta into fasttree to make a tree
+                LOGGER.debug("Window " + str(start_nucpos) + "-" + str(end_nucpos) +" satisfies window depth constraints")
+                LOGGER.debug("Start Fasttree for window " + str(start_nucpos) + "-" + str(end_nucpos))
                 window_prefix = os.path.splitext(msa_window_fasta_filename)[0]
                 fastree_logfilename = window_prefix + ".fasttree.log"
                 fastree_treefilename = window_prefix + ".tree"
@@ -82,8 +89,9 @@ def process_windows(ref_fasta_filename, sam_filename, out_dir, mapping_cutoff, r
                                            '-log', fastree_logfilename, '-out', fastree_treefilename,
                                            msa_window_fasta_filename],
                                           stdout=fasttree_stdouterr_fh, stderr=fasttree_stdouterr_fh, shell=False)
+                LOGGER.debug("Done Fasttree for window " + str(start_nucpos) + "-" + str(end_nucpos))
 
-
+                LOGGER.debug("Start HyPhy for window " + str(start_nucpos) + "-" + str(end_nucpos))
                 hyphy_modelfit_filename = window_prefix + ".nucmodelfit"
                 hyphy_dnds_tsv_filename = window_prefix + ".dnds.tsv"
                 pvalue = 0.95   # TODO:  do not hardcode
@@ -130,9 +138,12 @@ def process_windows(ref_fasta_filename, sam_filename, out_dir, mapping_cutoff, r
 
                     if hyphy_proc.returncode:
                         raise subprocess.CalledProcessError(cmd=hyphy_cmd, returncode=hyphy_proc.returncode)
+                LOGGER.debug("Done HyPhy for window " + str(start_nucpos) + "-" + str(end_nucpos))
 
+    LOGGER.debug("Start Ave Dn/DS for all windows")
     dnds_tsv_dir = os.path.dirname(hyphy_dnds_tsv_filename)
     ref2SeqDnDsInfo = slice_miseq.get_seq_dnds(dnds_tsv_dir=dnds_tsv_dir, pvalue_thresh=pvalue, ref_fasta_filename=ref_fasta_filename)
+    LOGGER.debug("Done Ave Dn/DS for all windows")
 
     return ref2SeqDnDsInfo
 
