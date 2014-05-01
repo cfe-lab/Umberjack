77516332edd6ee2509282c0443417b1e7783f8c2
Thuy Nguyen <tnguyen@cfenet.ubc.ca>
2014-04-30 18:06:00 -0700

    Temporarily comment out the unnecessary tests

--- test/Test_slice_miseq.py
+++ test/Test_slice_miseq.py
@@ -5,6 +5,7 @@ import sys
 import csv
 import array
 
+
 # For now, use a *.remap.sam file (paired end reads aligned to a consensus sequence with indels removed).
 SAM_FILENAME = "./simulations/data/sample_genomes.grinder-reads.rename.sam"
 MSA_FASTA_FILENAME = "./simulations/data/out/sample_genomes.grinder-reads.rename.msa.fasta"
@@ -50,57 +51,57 @@ class TestSliceMiSeq(unittest.TestCase):
             return site0based_to_dnds
 
 
-    def test_get_best_window_size_from_depth(self):
-        # TEST CASE:  test the algo for calculating best window size
-        # Uneven coverage, depth file has missing base positions (i.e.  base positions with zero coverage)
-        # Smaller window sizes are sufficient in some parts of the genome, but larger window sizes required for others.
-        # Must return the largest sufficient window size.
-        expected_windowsize = 9
-        depth_filename = "./data/Fake.depth"
-        cov_thresh = 50
-        check_windowsize = slice_miseq.get_best_window_size_from_depthfile(depth_filename=depth_filename, cov_thresh=cov_thresh) # TODO: implement
-        self.assertEqual(expected_windowsize, check_windowsize,
-                         msg="Expected windowsize=" + str(expected_windowsize) + " but got " + str(check_windowsize))
-
-
-    def test_get_best_window_size(self):
-        # TEST CASE:  test conversion form sam to bam to depth file, then obtain best window size
-        expected_window_size = 6  # TODO:  get the actual expected num
-        check_windowsize = slice_miseq.get_best_window_size_by_read_cov(sam_filename=SAM_FILENAME, ref_filename=REFERENCE_FASTA, cov_thresh=26)
-        self.assertEqual(check_windowsize, expected_window_size, "Expected window size=" + str(expected_window_size) +
-                        " but got " + str(check_windowsize))
-
-
-    def test_slice_msa_fasta(self):
-        # TEST CASE:  test that the sliced file is written properly to file
-        start_pos = 8598
-        end_pos = 8897
-        expected_total_seq = 639
-        slice_fasta_filename = "./simulations/data/sample_genomes.grinder-reads.rename.msa." + str(start_pos) + "_" + str(end_pos) + ".fasta"
-
-        total_seq = slice_miseq.create_slice_msa_fasta(fasta_filename=MSA_FASTA_FILENAME, out_fasta_filename=slice_fasta_filename,
-                                                       start_pos=start_pos, end_pos=end_pos, breadth_thresh=BREADTH_THRESH)
-
-        self.assertTrue(os.path.exists(slice_fasta_filename) and os.path.getsize(slice_fasta_filename) > 0,
-                        msg="Unable to write to sliced multiple-sequence aligned fasta file")
-        self.assertEqual(total_seq, expected_total_seq, msg="Expected total sequences written to file = " +
-                                                            str(expected_total_seq) + " but got " + str(total_seq))
-
-        expected_header = ">40607"
-        expected_sequence = "CCAAGGCCTAATTTCGACTAACTCCAGACACTTCTCTGGGTTANC-TCGCTGGNGGANGCG-TAGNGGANATNGGAGNAACGNCNTNTNGTAATCTGACNNNNGTNTACTTNATNGTTGGTNGATCGTGTGNGGAACAACAGTNCATTGGCGTCGTAAGACCAAGATCATTGAGNGGTAACGATATTACCGCCATACCGAATAAGACGATGACGGTAACAGGTGCCTCCCTGCCGTCCGTGGTCTTTCCTAACAGCCGCCTTTGCGCTGCCAC---------------------------"
-        is_found_expected_header = False
-
-        with open(slice_fasta_filename, 'r') as slice_fasta_fh:
-            for line in slice_fasta_fh:
-                line = line.rstrip()
-                if line.split()[0] == expected_header:
-                    is_found_expected_header = True
-                    check_seq = slice_fasta_fh.next().rstrip()
-                    self.assertEqual(check_seq, expected_sequence,
-                                     msg="Expected sequence " + expected_header + " to be " + expected_sequence + " but got " + check_seq)
-
-        self.assertTrue(is_found_expected_header, msg="Expected to find seq " + expected_header + " but didn't")
-
+    # def test_get_best_window_size_from_depth(self):
+    #     # TEST CASE:  test the algo for calculating best window size
+    #     # Uneven coverage, depth file has missing base positions (i.e.  base positions with zero coverage)
+    #     # Smaller window sizes are sufficient in some parts of the genome, but larger window sizes required for others.
+    #     # Must return the largest sufficient window size.
+    #     expected_windowsize = 9
+    #     depth_filename = "./data/Fake.depth"
+    #     cov_thresh = 50
+    #     check_windowsize = slice_miseq.get_best_window_size_from_depthfile(depth_filename=depth_filename, cov_thresh=cov_thresh) # TODO: implement
+    #     self.assertEqual(expected_windowsize, check_windowsize,
+    #                      msg="Expected windowsize=" + str(expected_windowsize) + " but got " + str(check_windowsize))
+    #
+    #
+    # def test_get_best_window_size(self):
+    #     # TEST CASE:  test conversion form sam to bam to depth file, then obtain best window size
+    #     expected_window_size = 6  # TODO:  get the actual expected num
+    #     check_windowsize = slice_miseq.get_best_window_size_by_read_cov(sam_filename=SAM_FILENAME, ref_filename=REFERENCE_FASTA, cov_thresh=26)
+    #     self.assertEqual(check_windowsize, expected_window_size, "Expected window size=" + str(expected_window_size) +
+    #                     " but got " + str(check_windowsize))
+    #
+    #
+    # def test_slice_msa_fasta(self):
+    #     # TEST CASE:  test that the sliced file is written properly to file
+    #     start_pos = 8598
+    #     end_pos = 8897
+    #     expected_total_seq = 639
+    #     slice_fasta_filename = "./simulations/data/sample_genomes.grinder-reads.rename.msa." + str(start_pos) + "_" + str(end_pos) + ".fasta"
+    #
+    #     total_seq = slice_miseq.create_slice_msa_fasta(fasta_filename=MSA_FASTA_FILENAME, out_fasta_filename=slice_fasta_filename,
+    #                                                    start_pos=start_pos, end_pos=end_pos, breadth_thresh=BREADTH_THRESH)
+    #
+    #     self.assertTrue(os.path.exists(slice_fasta_filename) and os.path.getsize(slice_fasta_filename) > 0,
+    #                     msg="Unable to write to sliced multiple-sequence aligned fasta file")
+    #     self.assertEqual(total_seq, expected_total_seq, msg="Expected total sequences written to file = " +
+    #                                                         str(expected_total_seq) + " but got " + str(total_seq))
+    #
+    #     expected_header = ">40607"
+    #     expected_sequence = "CCAAGGCCTAATTTCGACTAACTCCAGACACTTCTCTGGGTTANC-TCGCTGGNGGANGCG-TAGNGGANATNGGAGNAACGNCNTNTNGTAATCTGACNNNNGTNTACTTNATNGTTGGTNGATCGTGTGNGGAACAACAGTNCATTGGCGTCGTAAGACCAAGATCATTGAGNGGTAACGATATTACCGCCATACCGAATAAGACGATGACGGTAACAGGTGCCTCCCTGCCGTCCGTGGTCTTTCCTAACAGCCGCCTTTGCGCTGCCAC---------------------------"
+    #     is_found_expected_header = False
+    #
+    #     with open(slice_fasta_filename, 'r') as slice_fasta_fh:
+    #         for line in slice_fasta_fh:
+    #             line = line.rstrip()
+    #             if line.split()[0] == expected_header:
+    #                 is_found_expected_header = True
+    #                 check_seq = slice_fasta_fh.next().rstrip()
+    #                 self.assertEqual(check_seq, expected_sequence,
+    #                                  msg="Expected sequence " + expected_header + " to be " + expected_sequence + " but got " + check_seq)
+    #
+    #     self.assertTrue(is_found_expected_header, msg="Expected to find seq " + expected_header + " but didn't")
+    #
 
     def test_get_seq_dnds(self):
         # TODO:  automate check output
